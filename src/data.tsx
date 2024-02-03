a3b91c77966b049497f288660162f85ae478be38
Santino Valenzuela <41457708+valenzsa@users.noreply.github.com>
2024-02-03 06:25:34 +0000

    Create sample data to use in the shopping cart

---
+++ src/data.tsx
@@ -0,0 +1,33 @@
+const cartItems = [
+  {
+    id: 1,
+    name: "Organic Cauliflower, 1 Each",
+    image: "https://m.media-amazon.com/images/I/817uoYw7ElL._SX679_.jpg",
+    price: 4.49,
+
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: "Organic Red Onion, 1 Each",
+    image: "https://m.media-amazon.com/images/I/81UeYuulNjL._SX679_.jpg",
+    price: 1.49,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: "Tomato Vine Conventional, 1 Each",
+    image: "https://m.media-amazon.com/images/I/71YDSZ+0TCL._SX679_.jpg",
+    price: 2.29,
+    quantity: 1,
+  },
+  {
+    id: 4,
+    name: "Organic Red Bell Pepper, 1 Each",
+    image: "https://m.media-amazon.com/images/I/71BTI3PDLFL._SX679_.jpg",
+    price: 4.49,
+    quantity: 1,
+  },
+];
+
+export default cartItems;
