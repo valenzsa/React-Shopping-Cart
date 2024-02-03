678aef2b2522f0f6098ddf2a029a800d8081b541
Santino Valenzuela <41457708+valenzsa@users.noreply.github.com>
2024-02-03 06:33:45 +0000

    Updated Context component to create context, set up useReducer

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,13 +1,44 @@
 import { createContext, useContext, useReducer } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
+import {
+  CLEAR_CART,
+  REMOVE_ITEM,
+  INCREASE_QUANTITY,
+  DECREASE_QUANTITY,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./actions";
 
-const GlobalContext = createContext();
+type IShoppingCart = {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  quantity: number;
+};
+
+type IShoppingCartContext = [IShoppingCart[], React.Dispatch<IShoppingCart[]>];
+
+const GlobalContext = createContext<IShoppingCartContext>([[], () => []]);
 
 export const useGlobalContext = () => {
   return useContext(GlobalContext);
 };
 
+const initialStateObject = {
+  isLoading: false,
+  cart: new Map(),
+};
+
 const Context = ({ children }) => {
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, initialStateObject);
+
+  return (
+    <GlobalContext.Provider value={{ ...state }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 };
 
 export default Context;
